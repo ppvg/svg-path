5ddaa1e9bf3569a4f5aaedc94fcb0c9703fd8e2a
Peter-Paul van Gemerden <info@ppvg.nl>
2013-11-30 16:41:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# svg-path
+
+Parse and manipulate SVG `<path>`s.
+
+## Usage example
+
+    var svgPath = require('svg-path')
+
+    var pathData = 'm100,1e2C125,100 130,110 150,150l-25-75z'
+
+    var path = svgPath(pathData) // creates a new svgPath.Path
+    console.log(path.content)
+    /*
+        [
+           { type: 'M', relative: false, x: 100, y: 100 },
+           { type: 'C', relative: false,
+             x1: 125, y1: 100, x2: 130, y2: 110, x: 150, y: 150 },
+           { type: 'L', relative: true,  x: -25, y: -75 },
+           { type: 'Z' }
+        ]
+    */
+
+    // Apply transformations:
+
+    path.abs()
+    path.matrix(1, 0, 0, 1, 100, 50) // same as translate(100, 50)
+
+    // Transformations are applied in-place. To retain the original, create a copy first:
+
+    var newPath = path.copy()
---
+++ test/matrix.js
@@ -0,0 +1,79 @@
+var test = require('tape')
+var Path = require('../lib/Path')
+
+// |a c e|
+// |b d f| is also expressed as [a b c d e f]
+// |0 0 1|
+//
+// (See http://www.w3.org/TR/SVG/coords.html#EstablishingANewUserSpace)
+
+test('identity matrix', function (t) {
+  var path = new Path([
+    { type: 'M', relative:false, x:0,  y:0  },
+    { type: 'L', relative:false, x:30, y:60 }
+  ])
+  // |1 0 0|
+  // |0 1 0|
+  // |0 0 1|
+  path.matrix(1, 0, 0, 1, 0, 0)
+  t.same(path.content,
+    [
+      { type: 'M', relative:false, x:0,  y:0  },
+      { type: 'L', relative:false, x:30, y:60 }
+    ])
+  t.end()
+})
+
+test('translation', function (t) {
+  var path = new Path([
+    { type: 'M', relative:false, x:0,  y:0  },
+    { type: 'L', relative:false, x:30, y:60 }
+  ])
+  // |1 0 tx|
+  // |0 1 ty|
+  // |0 0 1 |
+  path.matrix(1, 0, 0, 1, 33, 55)
+  t.same(path.content,
+    [
+      { type: 'M', relative:false, x:33, y:55  },
+      { type: 'L', relative:false, x:63, y:115 },
+    ])
+  t.end()
+})
+
+test('scaling', function (t) {
+  var path = new Path([
+    { type: 'M', relative:false, x:0,  y:0  },
+    { type: 'L', relative:false, x:30, y:60 }
+  ])
+  // |sx 0  0|
+  // |0  sy 0|
+  // |0  0  1|
+  path.matrix(2, 0, 0, 3, 0, 0)
+  t.same(path.content,
+    [
+      { type: 'M', relative:false, x:0,  y:0   },
+      { type: 'L', relative:false, x:60, y:180 },
+    ])
+  t.end()
+})
+
+test('x1,y1 and x2,y2', function (t) {
+  var path = new Path([
+    { type: 'M', relative:false, x:0, y:0 },
+    { type: 'C', relative:false, x1:10, y1:0, x2:20, y2:10, x:20, y:20 }
+  ])
+  path.matrix(1, 0, 0, 1, 10, 10) // translate 10,10
+  t.same(path.content,
+    [
+      { type: 'M', relative:false, x:10, y:10 },
+      { type: 'C', relative:false, x1:20, y1:10, x2:30, y2:20, x:30, y:30 }
+    ])
+  path.matrix(2, 0, 0, 3, 0, 0) // scale 2,3
+  t.same(path.content,
+    [
+      { type: 'M', relative:false, x:20, y:30 },
+      { type: 'C', relative:false, x1:40, y1:30, x2:60, y2:60, x:60, y:90 }
+    ])
+  t.end()
+})
